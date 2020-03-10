430033763f8e5a4b05dd60cd59345bc2229491cc
gy741 <gy741.kim@gmail.com>
2020-03-10 09:16:36 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+# Download latest Golang shell script
+# https://gist.github.com/n8henrie/1043443463a4a511acf98aaa4f8f0f69
+ubuntu() {
+	echo "Finding latest version of Go for AMD64...";
+	url="$(wget -qO- https://golang.org/dl/ | grep -oP 'https:\/\/dl\.google\.com\/go\/go([0-9\.]+)\.linux-amd64\.tar\.gz' | head -n 1 )";
+	latest="$(echo $url | grep -oP 'go[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2 )";
+	wget "${url}";
+	echo  "Download successful :" $url;
+	tar -C /usr/local -xzf go$latest.linux-amd64.tar.gz;
+	mkdir $HOME/go;
+}
+
+# https://github.com/namhyung/uftrace/blob/master/misc/install-deps.sh
 if [ "x$(id -u)" != x0 ]; then
-    echo "You might have to run it as root user."
-    echo "Please run it again with 'sudo'."
-    echo
-    exit
+	echo "You might have to run it as root user."
+	echo "Please run it again with 'sudo'."
+	echo
+	exit
 fi
 
 OPT="${@}"
@@ -12,20 +25,9 @@ OPT="${@}"
 distro=$(grep "^ID=" /etc/os-release | cut -d\= -f2 | sed -e 's/"//g')
 
 case $distro in
-    "ubuntu")
-	ubuntu;;
-    #centos")
-    #   centos;;
-    *) # we can add more install command for each distros.
-        echo "\"$distro\" is not supported distro, so please install packages manually." ;;
+	"ubuntu")
+		apt-get $OPT install sqlite git gcc make wget;;
+		ubuntu;;
+	*) # we can add more install command for each distros.
+		echo "\"$distro\" is not supported distro, so please install packages manually." ;;
 esac
-
-
-ubuntu() {
-		#Download Latest Go
-		GOURLREGEX='https://dl.google.com/go/go[0-9\.]+\.linux-amd64.tar.gz'
-		echo "Finding latest version of Go for AMD64..."
-		url="$(wget -qO- https://golang.org/dl/ | grep -oP 'https:\/\/dl\.google\.com\/go\/go([0-9\.]+)\.linux-amd64\.tar\.gz' | head -n 1 )"
-		latest="$(echo $url | grep -oP 'go[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2 )"
-		wget "${url}"
-}
